a4e653a939a164c8686bc59f41c1d9ba40775722
Kazi Uddin <uddinimtiaz30@gmail.com>
2021-05-11 10:35:52 +0100

    asd

---
+++ exercise 1/app1.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return 'This is the home page'
+
+@app.route('/about')
+def about():
+    return 'This is the about page'
+@app.route('/about/<int:n>')
+def about1(n):
+    return str(n*n)
+
+if __name__ == "__main__":
+    app.run(debug=True)
